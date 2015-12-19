99eb062165d97ce0633e7037b7e08fb486479fac
Bruce Cowan <bruce@bcowan.me.uk>
2015-12-20 00:05:20 +0800

    Update youtube.sh

--- ffmpeg/youtube.sh
+++ ffmpeg/youtube.sh
@@ -10,6 +10,13 @@ if [ -z "$1" ]; then
 	echo "Needs a file to convert" >&2
 	exit 1
 else
-	# Ratefactor can be varied according to taste
-	ffmpeg -i "$INPUT" -c:a libopus -b:a 128k -c:v libx264 -crf 25 -sn $OUTPUT.mkv
+	# See https://support.google.com/youtube/answer/1722171?hl=en
+	# Summary:
+	# ffmpeg native AAC encoder, set for stereo recommended bitrate
+	# -preset slow -> Set to the slowest preset that you can be bothered with
+	# -crf 29 -> Somewhat arbitrary, see document above for bitrate guidance, CRF mode should be better quality than 1 pass VBR
+	# -g 12 -> GOP size of 12, should be half of framerate
+	# -bf 2 -> Number of consecutive b frames
+	# -sc_threshold -> closed GOP
+	ffmpeg -i "$INPUT" -c:a aac -strict -2 -b:a 384k -c:v libx264 -preset slow -crf 29 -g 12 -bf 2 -sc_threshold 0 $OUTPUT.mp4
 fi
